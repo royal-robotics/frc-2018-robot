0653885ca7329a9396361467f0670a33b9943da8
Connor Blomquist <connor.blomquists@gmail.com>
2018-01-09 19:59:28 -0800

    simple teleop drive

--- src/main/java/frc/team2522/robot/Robot.java
+++ src/main/java/frc/team2522/robot/Robot.java
@@ -2,9 +2,23 @@ package frc.team2522.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Sendable;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot {
+
+    Joystick leftStick = new Joystick(0);
+    Joystick rightStick = new Joystick(1);
+
+    VictorSP leftDrive1 = new VictorSP(0);
+    VictorSP leftDrive2 = new VictorSP(1);
+    VictorSP rightDrive1 = new VictorSP(2);
+    VictorSP rightDrive2 = new VictorSP(3);
+
+    public RobotDrive myDrive = new RobotDrive(leftDrive1, leftDrive2, rightDrive1, rightDrive2);
+
     @Override
     public void robotInit() {
         SmartDashboard.putString("test/getRobotValue", "Hello, here's a number: " + (new java.util.Random()).nextInt());
@@ -29,7 +43,13 @@ public class Robot extends IterativeRobot {
     public void autonomousPeriodic() { }
 
     @Override
-    public void teleopPeriodic() { }
+    public void teleopPeriodic() {
+        double leftPower = leftStick.getRawAxis(3);
+        double rightPower = rightDrive1.get();
+
+        //myDrive.tankDrive(leftPower, rightPower);
+        myDrive.tankDrive(leftStick, rightStick, /*squaredInputs*/ true);
+    }
 
     @Override
     public void testPeriodic() { }
