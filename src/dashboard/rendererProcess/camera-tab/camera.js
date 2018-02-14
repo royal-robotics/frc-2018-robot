a9d074829ba4c6956329ba4558e320441f417efc
Jonathan Phippen <jphippen@cs.washington.edu>
2018-02-13 20:08:16 -0800

    Untested fix for camera image attempt without robot connection

--- src/dashboard/rendererProcess/camera-tab/camera.js
+++ src/dashboard/rendererProcess/camera-tab/camera.js
@@ -1,11 +1,39 @@
-
 $(() => {
     var baseUrl = "http://roborio-2522-frc.local:1181";
-    var streamUrl = `${baseUrl}/stream.mjpg`
-    var settingsUrl = `${baseUrl}/settings.json`
+    var streamUrl = `${baseUrl}/stream.mjpg`;
+    var settingsUrl = `${baseUrl}/settings.json`;
 
     //$("#camera").css('background-image', `url("${streamUrl}")`);
     console.log(`url(${streamUrl})`);
     // $("#camera").css('width', '640px');
     // $("#camera").css('height', '480px');
+
+    if (NetworkTables.isRobotConnected()) {
+        connectToCamera();
+    }
 });
+
+ipc.on("connected", (ev, con) => {
+    if (con) {
+        connectToCamera();
+    } else {
+        disconnectFromCamera();
+    }
+});
+
+function connectToCamera() {
+    console.log("Connecting to camera...");
+    replaceImage("http://roborio-2522-frc.local:1181/stream.mjpg");
+}
+
+function disconnectFromCamera() {
+    console.log("Disconnecting from camera...");
+    replaceImage("images/noCamera.png");
+}
+
+function replaceImage(src) {
+    $("#camera").empty();
+    let stream = document.createElement("img");
+    stream.src = src;
+    $("#camera").append(stream);
+}
