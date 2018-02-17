953c8a7a8e48b656abd8b30f8f39d116d5b0c3a3
Jonathan Phippen <jphippen@cs.washington.edu>
2018-02-16 20:58:20 -0800

    Fixed camera urls and fixed running partial js multiple times

--- src/dashboard/rendererProcess/camera/camera.js
+++ src/dashboard/rendererProcess/camera/camera.js
@@ -1,4 +1,11 @@
+// Camera debug functionality
 $(() => {
+    // Prevent code from running multiple times
+    ipc.send("camera-load");
+});
+
+ipc.on("camera-load-continue", () => {
+    console.log("loaded");
     var baseUrl = "http://roborio-2522-frc.local:1181";
     var streamUrl = `${baseUrl}/stream.mjpg`;
     var settingsUrl = `${baseUrl}/settings.json`;
@@ -11,29 +18,30 @@ $(() => {
     if (NetworkTables.isRobotConnected()) {
         connectToCamera();
     }
-});
 
-ipc.on("connected", (ev, con) => {
-    if (con) {
-        connectToCamera();
-    } else {
-        disconnectFromCamera();
+    ipc.on("connected", (ev, con) => {
+        if (con) {
+            connectToCamera();
+        } else {
+            disconnectFromCamera();
+        }
+    });
+
+    function connectToCamera() {
+        console.log("Connecting to camera...");
+        replaceImage("http://roborio-2522-frc.local:1181/stream.mjpg");
+    }
+
+    function disconnectFromCamera() {
+        console.log("Disconnecting from camera...");
+        replaceImage("images/noCamera.png");
+    }
+
+    function replaceImage(src) {
+        console.log($("#camera"))
+        $("#camera").empty();
+        let img = document.createElement("img");
+        img.src = src;
+        $("#camera").append(img);
     }
 });
-
-function connectToCamera() {
-    console.log("Connecting to camera...");
-    replaceImage("http://roborio-2522-frc.local:1181/stream.mjpg");
-}
-
-function disconnectFromCamera() {
-    console.log("Disconnecting from camera...");
-    replaceImage("images/noCamera.png");
-}
-
-function replaceImage(src) {
-    $("#camera").empty();
-    let stream = document.createElement("img");
-    stream.src = src;
-    $("#camera").append(stream);
-}
