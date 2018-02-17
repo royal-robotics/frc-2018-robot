df097135ec2e2ede2a84bef9188f9356813f26ae
Connor Blomquist <connor.blomquists@gmail.com>
2018-02-17 15:33:29 -0800

    remove some commented out code

--- src/main/java/frc/team2522/robot/Robot.java
+++ src/main/java/frc/team2522/robot/Robot.java
@@ -62,40 +62,18 @@ public class Robot extends IterativeRobot {
 
     Drivebase drivebase = new Drivebase();
 
-//    CvSink cvSink;
-//    CvSource outputStream;
-//
-//    Mat source = new Mat();
-//    Mat output = new Mat();
-
     CameraPipeline camera = new CameraPipeline(leftStick);
 
     @Override
     public void robotInit() {
-//        CameraServer.getInstance().startAutomaticCapture();
-//
-//        cvSink = CameraServer.getInstance().getVideo();
-//        outputStream = new CvSource("blur", VideoMode.PixelFormat.kMJPEG, 640, 480, 30);
-//
-//        MjpegServer mjpegServer2 = CameraServer.getInstance().addServer("server_blur", 1182);
-//        mjpegServer2.setSource(outputStream);
 
-        SmartDashboard.putNumber("Servo/angle", 0);
-
-        SmartDashboard.putString("example/test-string", "hello world");
-        SmartDashboard.putBoolean("example/test-boolean", true);
-        SmartDashboard.putNumber("example/test-number", 42);
     }
 
     @Override
     public void disabledInit() { }
 
     @Override
-    public void disabledPeriodic() {
-//        cvSink.grabFrame(source);
-//        Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
-//        outputStream.putFrame(output);
-    }
+    public void disabledPeriodic() { }
 
     @Override
     public void autonomousInit() { }
