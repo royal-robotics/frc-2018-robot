37bc01c87e7e0b1f7177c848ad5536ce26d7b6aa
Connor Blomquist <connor.blomquists@gmail.com>
2018-02-19 16:35:45 -0800

    WIP LiftData

--- src/main/java/frc/team2522/robot/subsystems/Elevator/Lift/Lift.java
+++ src/main/java/frc/team2522/robot/subsystems/Elevator/Lift/Lift.java
@@ -14,7 +14,7 @@ public class Lift {
 
     DoubleSolenoid brake = new DoubleSolenoid(0, 1, 6);
 
-    Encoder encoder = new Encoder(14, 15);
+
 
     Joystick driver;
 
@@ -24,20 +24,9 @@ public class Lift {
 
         lift2.follow(liftMotors);
         lift3.follow(liftMotors);
-
-        //TODO: Figure this out
-        final double inchesPerPulse = 0.1;
-        encoder.setDistancePerPulse(inchesPerPulse);
-        encoder.setReverseDirection(false);
-        encoder.reset();
     }
 
     public void fmsUpdateTeleop() {
-        driver.setRumble(GenericHID.RumbleType.kLeftRumble, 1.0);
-        driver.setRumble(GenericHID.RumbleType.kRightRumble, 1.0);
-
-        System.out.println("Lift Encoder: " + encoder.getDistance());
-
         if(driver.getPOV() == 0) {
             liftMotors.set(ControlMode.PercentOutput, 0.8);
         } else if(driver.getPOV() == 180) {
--- src/main/java/frc/team2522/robot/subsystems/Elevator/Lift/LiftData.java
+++ src/main/java/frc/team2522/robot/subsystems/Elevator/Lift/LiftData.java
@@ -2,11 +2,56 @@ package frc.team2522.robot.subsystems.Elevator.Lift;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
+import frc.team2522.robot.libs.CircularList;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class LiftData {
+    private final int msUpdate = 10;
 
-    //DigitalInput lift
+    //Average velocity state
+    private Timer timer = new Timer();
+    private double lastPosition = 0;
+    private CircularList<Double> lastPositionDiffs = new CircularList<>(10);
+    CircularList<Double> lastVelocities = new CircularList<>(10);
+
+    //Robot components
+    private Encoder encoder = new Encoder(14, 15);
+    private DigitalInput hallEffect = new DigitalInput(0);
+
     public LiftData() {
+        // TODO: The spool is actually bigger when the lift is down since the cord increases
+        // the diameter. We should adjust this value as the lift moves.
+        final double inchesPerPulse = (3.5 * Math.PI) / 256;
 
+        encoder.setDistancePerPulse(inchesPerPulse);
+        encoder.setReverseDirection(false);
+        encoder.reset();
+
+        timer.scheduleAtFixedRate(new TimerTask() {
+            public void run() {
+                tick();
+            }
+        }, msUpdate, msUpdate);
+    }
+
+    private void tick() {
+        //TODO: use System.nanoTime to get a more precise time measurement
+        double newPosition = encoder.getDistance();
+        double changeInPosition = newPosition - lastPosition;
+        lastPositionDiffs.add(changeInPosition);
+        lastPosition = newPosition;
+
+        double newVelocity = changeInPosition / (msUpdate / 1000.0);
+        lastVelocities.add(newVelocity);
+    }
+
+    public double getPosition() {
+        return 0;
+    }
+
+    public double getVelocity() {
+        return 0;
     }
 }
