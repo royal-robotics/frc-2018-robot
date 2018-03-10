9c7ae243f72e400ddd5f89da1ce227d96cfa3357
Connor Blomquist <connor.blomquists@gmail.com>
2018-03-10 01:01:20 -0800

    WIP missed files

---
+++ src/dashboard/rendererProcess/camera/camera-connect.js
@@ -0,0 +1,33 @@
+var baseUrl = "http://roborio-2522-frc.local:1181";
+var streamUrl = `${baseUrl}/stream.mjpg`;
+var settingsUrl = `${baseUrl}/settings.json`;
+
+
+$(() => {
+    ipc.on('connected', (ev, isConnected) => connectionChange(isConnected));
+    connectionChange(NetworkTables.isRobotConnected());
+});
+
+function connectionChange(isConnected) {
+    if(isConnected)
+        connectToCamera();
+    else
+        disconnectFromCamera();
+}
+
+function connectToCamera() {
+    console.log("Connecting to camera...");
+    replaceImage("http://roborio-2522-frc.local:1181/stream.mjpg");
+}
+
+function disconnectFromCamera() {
+    console.log("Disconnecting from camera...");
+    replaceImage("images/noCamera.png");
+}
+
+function replaceImage(src) {
+    $("#camera").empty();
+    let img = document.createElement("img");
+    img.src = src;
+    $("#camera").append(img);
+}
---
+++ src/dashboard/rendererProcess/camera/camera-settings.js
@@ -0,0 +1,54 @@
+var baseUrl = "http://roborio-2522-frc.local:1181";
+var streamUrl = `${baseUrl}/stream.mjpg`;
+var settingsUrl = `${baseUrl}/settings.json`;
+
+
+$(() => {
+    $("#detector").change(updateValues);
+    $("#filters").change(updateValues);
+
+    $("#blur-kernel").change(updateValues);
+    $("#blur-sigma").change(updateValues);
+
+    ipc.on('connected', (ev, isConnected) => updateValues());
+});
+
+
+function updateValues() {
+    NetworkTables.putValue("/SmartDashboard/Camera/Detector", $("#detector option:selected").val());
+    NetworkTables.putValue("/SmartDashboard/Camera/Filter", $("#filters option:selected").val());
+
+    console.log("kernel: " + $("#blur-kernel").val());
+    console.log("sigma: " + $("#blur-sigma").val() );
+
+    NetworkTables.putValue("/SmartDashboard/Camera/Filter/blob/blur/kernelSize", $("#blur-kernel").val());
+    NetworkTables.putValue("/SmartDashboard/Camera/Filter/blob/blur/sigma", $("#blur-sigma").val());
+}
+
+$(() => {
+    setupDashboardColorPicker("colorpicker-low", "/SmartDashboard/Camera/Filter/blob/color/low");
+    setupDashboardColorPicker("colorpicker-high", "/SmartDashboard/Camera/Filter/blob/color/high");
+});
+
+
+function setupDashboardColorPicker(id, key) {
+    $(`#${id}`).colorpicker({
+        parts:  ['bar', 'hsv', 'rgb', 'map'],
+        layout: {
+            bar:        [0, 0, 1, 4],
+            map:		[1, 0, 1, 5],
+            rgb:        [2, 0, 1, 1],
+            hsv:        [2, 1, 1, 1],
+        },
+        "select": function(ev, data) {
+            var h = Math.round(data.hsv.h * 255);
+            var s = Math.round(data.hsv.s * 255);
+            var v = Math.round(data.hsv.v * 255);
+
+            $(`#${id}`).val(`H: ${h} S: ${s} V: ${v}`);
+            NetworkTables.putValue(`${key}/h`, h);
+            NetworkTables.putValue(`${key}/s`, s);
+            NetworkTables.putValue(`${key}/v`, v);
+        }
+    });
+}
